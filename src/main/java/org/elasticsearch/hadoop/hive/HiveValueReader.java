e3e2b74f30c0401492831036c7950dda74c08251
Costin Leau <costin.leau@gmail.com>
2013-08-05 19:20:15 +0300

    improve hive value reader when dealing w/ nested dates

--- src/main/java/org/elasticsearch/hadoop/hive/HiveValueReader.java
+++ src/main/java/org/elasticsearch/hadoop/hive/HiveValueReader.java
@@ -5,6 +5,7 @@ import java.sql.Timestamp;
 import javax.xml.bind.DatatypeConverter;
 
 import org.apache.hadoop.hive.serde2.io.TimestampWritable;
+import org.apache.hadoop.io.Writable;
 import org.elasticsearch.hadoop.mr.WritableValueReader;
 
 public class HiveValueReader extends WritableValueReader {
@@ -12,5 +13,10 @@ public class HiveValueReader extends WritableValueReader {
     @Override
     protected Object date(String value) {
         return new TimestampWritable(new Timestamp(DatatypeConverter.parseDateTime(value).getTimeInMillis()));
+    }
+
+    @Override
+    protected Class<? extends Writable> dateType() {
+        return TimestampWritable.class;
     }
 }
