90c6c69cb65915a6f2fd6efc3b7db7c30c45cbab
Costin Leau <costin.leau@gmail.com>
2013-08-05 23:02:35 +0300

    use joda optional dateTimeparser instead of dateOptionalTime one

--- src/main/java/org/elasticsearch/hadoop/pig/DateConverter.java
+++ src/main/java/org/elasticsearch/hadoop/pig/DateConverter.java
@@ -11,47 +11,48 @@ import org.joda.time.format.ISODateTimeFormat;
 
 class DateConverter {
 
-	private static boolean pig11Available;
-	
-	static {
-		// initialize
-		pig11Available = "datetime".equals(DataType.findTypeName((byte) 30));
-	}
-	
-	static String convertToES(Object pigDate) {
-		return (pig11Available ? Pig11OrHigherConverter.convertToES(pigDate) : PigUpTo10Converter.convertToES(pigDate));
-	}
-	
-	static Object convertFromES(String esDate) {
-		return (pig11Available ? Pig11OrHigherConverter.convertFromES(esDate) : PigUpTo10Converter.convertFromES(esDate));
-	}
-	
-	private static abstract class PigUpTo10Converter {
-		static String convertToES(Object pigDate) {
-			if (pigDate instanceof Number) {
-				Calendar cal = Calendar.getInstance();
-				cal.setTimeInMillis(((Number) pigDate).longValue());
-				return DatatypeConverter.printDateTime(cal);
-			}
-			if (pigDate instanceof String) {
-				return ((String) pigDate);
-			}
-			throw new IllegalArgumentException(String.format("Cannot convert [%s] to date", pigDate));
-		}
-		
-		static Object convertFromES(String esDate) {
-			return DatatypeConverter.parseDateTime(esDate).getTimeInMillis();
-		}
-	}
-	
-	private static abstract class Pig11OrHigherConverter {
-		static String convertToES(Object pigDate) {
-			DateTime dt = (DateTime) pigDate;
-			return dt.toString(ISODateTimeFormat.dateOptionalTimeParser());
-		}
-		
-		static Object convertFromES(String esDate) {
-			return ISODateTimeFormat.dateOptionalTimeParser().parseDateTime(esDate);
-		}
-	}
+    private static boolean pig11Available;
+
+    static {
+        // initialize
+        pig11Available = "datetime".equals(DataType.findTypeName((byte) 30));
+    }
+
+    static String convertToES(Object pigDate) {
+        return (pig11Available ? Pig11OrHigherConverter.convertToES(pigDate) : PigUpTo10Converter.convertToES(pigDate));
+    }
+
+    static Object convertFromES(String esDate) {
+        return (pig11Available ? Pig11OrHigherConverter.convertFromES(esDate) : PigUpTo10Converter.convertFromES(esDate));
+    }
+
+    private static abstract class PigUpTo10Converter {
+        static String convertToES(Object pigDate) {
+            if (pigDate instanceof Number) {
+                Calendar cal = Calendar.getInstance();
+                cal.setTimeInMillis(((Number) pigDate).longValue());
+                return DatatypeConverter.printDateTime(cal);
+            }
+            if (pigDate instanceof String) {
+                return ((String) pigDate);
+            }
+            throw new IllegalArgumentException(String.format("Cannot convert [%s] to date", pigDate));
+        }
+
+        static Object convertFromES(String esDate) {
+            return DatatypeConverter.parseDateTime(esDate).getTimeInMillis();
+        }
+    }
+
+    private static abstract class Pig11OrHigherConverter {
+        static String convertToES(Object pigDate) {
+            DateTime dt = (DateTime) pigDate;
+            // ISODateTimeFormat.dateOptionalTimeParser() throws "printing not supported"
+            return dt.toString();
+        }
+
+        static Object convertFromES(String esDate) {
+            return ISODateTimeFormat.dateOptionalTimeParser().parseDateTime(esDate);
+        }
+    }
 }
