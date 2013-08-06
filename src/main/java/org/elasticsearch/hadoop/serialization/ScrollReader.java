64040a554db9b8200678b34b17bf3d9d1c8df2a6
Costin Leau <costin.leau@gmail.com>
2013-08-06 17:14:12 +0300

    fix incorrect terminator for json positioning

--- src/main/java/org/elasticsearch/hadoop/serialization/ScrollReader.java
+++ src/main/java/org/elasticsearch/hadoop/serialization/ScrollReader.java
@@ -168,7 +168,7 @@ public class ScrollReader {
             String nodeMapping = fieldMapping;
 
             if (nodeMapping != null) {
-                nodeMapping = fieldMapping + "." + currentName;
+                nodeMapping = fieldMapping + "/" + currentName;
             }
             else {
                 nodeMapping = currentName;
