d0e1834fabc28a7e753ce59652d45fdbe23a8073
Costin Leau <costin.leau@gmail.com>
2013-08-07 17:52:08 +0300

    add missing package info (a lot more to go)

---
+++ src/main/java/org/elasticsearch/hadoop/mr/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Package providing integration between Elasticsearch and
+ * <a href="http://hadoop.apache.org/docs/stable/mapred_tutorial.html">Map/Reduce</a>.
+ *
+ * <p/>
+ *
+ * Defaults and settings can be configured through Hadoop configuration object.
+ *
+ * <br/>
+ * These are:
+ * <pre>
+ * 	es.host - default ES host, used by the Map/Reduce Input/OutputFormat
+ *  es.port - default ES port, used by the Map/Reduce Input/OutputFormat
+ * </pre>
+ *
+ */
+package org.elasticsearch.hadoop.mr;
