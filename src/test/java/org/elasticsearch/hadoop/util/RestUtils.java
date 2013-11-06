45e95ab538807a1f4cc56ad27633a78901f182ae
Costin Leau <costin.leau@gmail.com>
2013-11-06 23:10:03 +0200

    add missing class relates to #66 #85 #81 #69

---
+++ src/test/java/org/elasticsearch/hadoop/util/RestUtils.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.elasticsearch.hadoop.util;
+
+import org.elasticsearch.hadoop.rest.RestClient;
+
+public class RestUtils {
+
+    public static void putMapping(String index, byte[] content) throws Exception {
+        RestClient rc = new RestClient(new TestSettings());
+        BytesArray bs = new BytesArray(content);
+        rc.putMapping(index, index + "/_mapping", bs.bytes);
+        rc.close();
+    }
+
+    public static void putMapping(String index, String location) throws Exception {
+        putMapping(index, TestUtils.fromInputStream(RestUtils.class.getClassLoader().getResourceAsStream(location)));
+    }
+}
