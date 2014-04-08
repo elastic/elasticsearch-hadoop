b012531ed506cf638bd2558f0ba962d6a72ad859
Costin Leau <costin.leau@gmail.com>
2014-04-08 16:00:39 +0300

    Various build improvements

--- hive/src/main/java/org/elasticsearch/hadoop/hive/package-info.java
+++ hive/src/main/java/org/elasticsearch/hadoop/hive/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Integration for <a href="http://hive.apache.org/">Apache Hive</a>,
+ * Integration for <a href="http://hive.apache.org/">Apache Hive</a>.
  * <p/>
  * Exposes Elasticsearch as tables which can be natively updated or queried through HiveQL.
  */
