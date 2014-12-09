c3dde8c9c91861734c9015166f4b93c56cd3ee28
Costin Leau <costin.leau@gmail.com>
2014-12-10 00:55:34 +0200

    [PIG] Document work-around for Pig 1 reducer problem
    Since in many cases Pig internal assumptions force the use of 1 reducer,
    no matter the job properties or the number of splits, document the
    settings that disable this behavior (and which unfortunately cannot be
    set directly by es-hadoop).
    
    fix #294
    
    (cherry picked from commit 3f7aee4574d881ef83aa657a4fc72ae100cc4a4f)

--- pig/src/main/java/org/elasticsearch/hadoop/pig/EsStorage.java
+++ pig/src/main/java/org/elasticsearch/hadoop/pig/EsStorage.java
@@ -358,7 +358,6 @@ public class EsStorage extends LoadFunc implements LoadMetadata, LoadPushDown, S
     }
 
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
     private void extractProjection(Configuration cfg) throws IOException {
         String fields = getUDFProperties().getProperty(InternalConfigurationOptions.INTERNAL_ES_TARGET_FIELDS);
         if (fields != null) {
