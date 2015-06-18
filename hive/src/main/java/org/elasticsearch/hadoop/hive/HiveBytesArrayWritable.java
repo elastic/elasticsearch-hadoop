f1b61183bb98ef0810baf817aac8abfc8cdeadd4
Costin Leau <costin.leau@gmail.com>
2015-06-18 19:11:11 +0300

    Re-add jetty as it looks like the CI fails without it

--- hive/src/main/java/org/elasticsearch/hadoop/hive/HiveBytesArrayWritable.java
+++ hive/src/main/java/org/elasticsearch/hadoop/hive/HiveBytesArrayWritable.java
@@ -36,16 +36,18 @@ import org.elasticsearch.hadoop.util.StringUtils;
  */
 public class HiveBytesArrayWritable extends BinaryComparable implements WritableComparable<BinaryComparable>, Serializable {
 
-    private BytesArray ba;
+    private transient BytesArray ba;
 
     public HiveBytesArrayWritable() {
         ba = null;
     }
 
+    @Override
     public int getLength() {
         return (ba != null ? ba.length() : 0);
     }
 
+    @Override
     public byte[] getBytes() {
         return (ba != null ? ba.bytes() : BytesArray.EMPTY);
     }
@@ -58,6 +60,7 @@ public class HiveBytesArrayWritable extends BinaryComparable implements Writable
         return ba;
     }
 
+    @Override
     public void readFields(DataInput in) throws IOException {
         int size = in.readInt();
         byte[] bytes = new byte[size];
@@ -65,6 +68,7 @@ public class HiveBytesArrayWritable extends BinaryComparable implements Writable
         ba = new BytesArray(bytes);
     }
 
+    @Override
     public void write(DataOutput out) throws IOException {
         int size = (ba != null ? ba.length() : 0);
         byte[] bytes = (ba != null ? ba.bytes() : BytesArray.EMPTY);
@@ -76,6 +80,7 @@ public class HiveBytesArrayWritable extends BinaryComparable implements Writable
     /**
      * Are the two byte sequences equal?
      */
+    @Override
     public boolean equals(Object right_obj) {
         if (right_obj instanceof HiveBytesArrayWritable)
             return super.equals(right_obj);
@@ -90,6 +95,7 @@ public class HiveBytesArrayWritable extends BinaryComparable implements Writable
     /**
      * Generate the stream of bytes as hex pairs separated by ' '.
      */
+    @Override
     public String toString() {
         return (ba != null ? StringUtils.asUTFString(ba.bytes(), 0, ba.length()) : "");
     }
@@ -103,6 +109,7 @@ public class HiveBytesArrayWritable extends BinaryComparable implements Writable
         /**
          * Compare the buffers in serialized form.
          */
+        @Override
         public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
             return compareBytes(b1, s1 + 4, l1 - 4, b2, s2 + 4, l2 - 4);
         }
