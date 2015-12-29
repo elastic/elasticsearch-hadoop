791fc477d99612ce9a19af6c2f005920ff7acc18
Costin Leau <costin.leau@gmail.com>
2015-12-29 23:58:18 +0200

    [HIVE] Fix incorrect field resolving
    relates #605

--- hive/src/main/java/org/elasticsearch/hadoop/hive/HiveFieldExtractor.java
+++ hive/src/main/java/org/elasticsearch/hadoop/hive/HiveFieldExtractor.java
@@ -40,7 +40,7 @@ public class HiveFieldExtractor extends ConstantFieldExtractor {
 
     @Override
     protected Object extractField(Object target) {
-        List<String> flNames = getFieldNames();
+        List<String> flNames = fieldNames;
 
         for (int i = 0; i < flNames.size(); i++) {
             String fl = flNames.get(i);
@@ -52,7 +52,7 @@ public class HiveFieldExtractor extends ConstantFieldExtractor {
                     StructField field = soi.getStructFieldRef(fl);
                     ObjectInspector foi = field.getFieldObjectInspector();
                     Assert.isTrue(foi.getCategory() == ObjectInspector.Category.PRIMITIVE,
-                            String.format("Field [%s] needs to be a primitive; found [%s]", fieldNames, foi.getTypeName()));
+                            String.format("Field [%s] needs to be a primitive; found [%s]", fl, foi.getTypeName()));
 
                     // expecting a writeable - simply do a toString
                     target = soi.getStructFieldData(type.getObject(), field);
