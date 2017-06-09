f09aab84c1b3a240cbcc3a77836bc09610ffe0ed
James Baiera <james.baiera@gmail.com>
2017-06-09 14:29:27 -0400

    Introduce Gradle buildSrc and prepare the project for a new release manager system.
    Moving elasticsearch fixture code to a proper plugin.
    Change names of cluster properties.
    Remove def types and set types in method signatures.
    Dynamically choose whether or not we are building a snapshot at the commandline.
    Adding a writePom task that exports a pom to artifacts.
    Added a step to the pack task that copies the jars to the artifacts directory.
    Assemble depends on writePom now, and also copies the libs to the artifact directory.
    Move test jar to build instead of assemble. Change artifacts directory to distributions.

---
+++ buildSrc/src/main/groovy/org/elasticsearch/hadoop/gradle/VersionProperties.groovy
@@ -0,0 +1,25 @@
+package org.elasticsearch.hadoop.gradle
+
+/**
+ * Loads the locally available version information from the build source.
+ */
+class VersionProperties {
+
+    public static final String ESHADOOP_VERSION
+    public static final String ELASTICSEARCH_VERSION
+    public static final Map<String, String> VERSIONS
+    static {
+        Properties versionProperties = new Properties()
+        InputStream propertyStream = VersionProperties.class.getResourceAsStream('/version.properties')
+        if (propertyStream == null) {
+            throw new RuntimeException("Could not locate the version.properties file!")
+        }
+        versionProperties.load(propertyStream)
+        ESHADOOP_VERSION = versionProperties.getProperty('eshadoop')
+        ELASTICSEARCH_VERSION = versionProperties.getProperty('elasticsearch')
+        VERSIONS = new HashMap<>()
+        for (String propertyName: versionProperties.stringPropertyNames()) {
+            VERSIONS.put(propertyName, versionProperties.getProperty(propertyName))
+        }
+    }
+}
