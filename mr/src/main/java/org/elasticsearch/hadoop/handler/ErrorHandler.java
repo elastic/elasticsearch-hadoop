6a8e6827a01492c014a96c56dde63c5f7ad96aa7
James Baiera <james.baiera@gmail.com>
2017-11-10 14:39:54 -0500

    Ditch the redundant public modifiers and remove the abstract method check.

--- mr/src/main/java/org/elasticsearch/hadoop/handler/ErrorHandler.java
+++ mr/src/main/java/org/elasticsearch/hadoop/handler/ErrorHandler.java
@@ -11,7 +11,7 @@ public interface ErrorHandler<I extends Exceptional, O> {
      * Called at the handler creation time to initialize any internal state or resources.
      * @param properties Properties for this handler with handler name prefix stripped away.
      */
-    public void init(Properties properties);
+    void init(Properties properties);
 
     /**
      * Called when an exception or failure occurs in a part of the connector.
@@ -22,17 +22,10 @@ public interface ErrorHandler<I extends Exceptional, O> {
      * @throws Exception In the event that the current failure should not be handled, and should halt the connector
      * processing.
      */
-    public HandlerResult onError(I entry, ErrorCollector<O> collector) throws Exception;
+    HandlerResult onError(I entry, ErrorCollector<O> collector) throws Exception;
 
     /**
      * Called at the close of the connector to clean up any internal resources.
      */
-    public void close();
-
-    /**
-     * The error handlers take advantage of a lot of generics. Thus it is difficult to verify that you have provided
-     * type-safe implementations at runtime without checking that you have extended from one of the abstract classes.
-     */
-    public void __implement_the_abstract_class_instead__();
-
+    void close();
 }
