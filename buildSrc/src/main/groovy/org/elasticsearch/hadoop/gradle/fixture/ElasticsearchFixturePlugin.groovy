829fd8c40f1812f321e15226b3b16ec62fb5618a
James Baiera <james.baiera@gmail.com>
2018-02-21 14:17:04 -0500

    Allow the usage of a local repositry when building the project. Pull ES build artifacts from there when it exists.

--- buildSrc/src/main/groovy/org/elasticsearch/hadoop/gradle/fixture/ElasticsearchFixturePlugin.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/hadoop/gradle/fixture/ElasticsearchFixturePlugin.groovy
@@ -27,10 +27,12 @@ class ElasticsearchFixturePlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
+
         def version = project.hasProperty("es.version") ? project.getProperty("es.version") : project.elasticsearchVersion
 
         // Optionally allow user to disable the fixture
-        def useFixture = Boolean.parseBoolean(project.hasProperty("tests.fixture.es.enable") ? project.getProperty("tests.fixture.es.enable") : "true")
+        def hasLocalRepo = project.hasProperty("localRepo")
+        def useFixture = !hasLocalRepo && Boolean.parseBoolean(project.hasProperty("tests.fixture.es.enable") ? project.getProperty("tests.fixture.es.enable") : "true")
 
         if (useFixture) {
             // Depends on project already containing an "integrationTest"
