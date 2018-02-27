e1bf50fb2f07ade13ba41d3c9891339dd5d8392e
James Baiera <james.baiera@gmail.com>
2018-02-27 15:08:12 -0800

    Overhaul the build system to be modular.
    Pull repository settings into build plugin.
    Remove PMD plugin.
    Remove Findbugs and Jacoco. We'll add it back later if we want it.
    Add jbaiera to developer list.
    Use https links in pom.
    Point Hive docs to 1.2.2 because 1.2.1 is just gone.
    Include propdeps in build source.
    Burn S3 tasks to the ground.
    Remove maven deployment for upload archives.
    Ditch checksum operation on distzip.
    Ditch dist.gradle script.
    Add a root build plugin that defines and configures the master
    archive tasks. Add an integration plugin that configures the archive
    tasks on master with their own artifacts/sources/settings.
    Remove test java from test resource dirs.
    Convert spark13 to variant build. Revert back to spark 2.2.0.
    Assemble should only build the archives. Remove the javadoc and source
    jars from the archives configuration since we won't use upload archives
    anymore. Pack task still packages all jars. Add distribution task. The
    distribution task will take over the previous things that were heaped
    on the assemble task, like the distZip, writing poms, and moving
    artifacts.

---
+++ buildSrc/src/main/groovy/org/elasticsearch/hadoop/gradle/IntegrationBuildPlugin.groovy
@@ -0,0 +1,109 @@
+package org.elasticsearch.hadoop.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.ExternalDependency
+import org.gradle.api.file.CopySpec
+import org.gradle.api.file.DuplicatesStrategy
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.api.tasks.bundling.Zip
+import org.gradle.api.tasks.javadoc.Javadoc
+
+/**
+ * Plugin for configuring any project module that ends up packaged as part of the
+ * master distribution (all integrations combined).
+ * <p>
+ * Note: This is only for defining settings/tasks that have a root/subproject relationship that have to
+ * do with packaging. Tasks or settings that are subject to ALL projects (not just the ones that are shipped
+ * in the master project archive) should be configured in {@link BuildPlugin}.
+ *
+ * @see RootBuildPlugin
+ */
+class IntegrationBuildPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project target) {
+        // Ensure build commands/settings/tasks are on the root project before starting.
+        target.getRootProject().getPluginManager().apply(RootBuildPlugin.class)
+
+        // Ensure that the common build plugin is already applied
+        target.getPluginManager().apply(BuildPlugin.class)
+
+        configureProjectJars(target)
+        configureProjectZip(target)
+        configureRootProjectDependencies(target)
+    }
+
+    private static def configureProjectJars(Project project) {
+        // We do this after evaluation since the scala projects may change around what the final archive name is.
+        project.afterEvaluate {
+            // Add the sub-project's jar contents to the project's uber-jar
+            Jar rootJar = project.rootProject.getTasks().getByName('jar') as Jar
+            rootJar.dependsOn(project.tasks.jar)
+            rootJar.from(project.zipTree(project.tasks.jar.archivePath)) {
+                exclude "META-INF/**"
+                include "**/*"
+            }
+
+            // Add sources to root project's sources jar
+            Jar rootSourcesJar = project.rootProject.getTasks().getByName("sourcesJar") as Jar
+            rootSourcesJar.from(project.sourceSets.main.allJava.srcDirs)
+
+            // Configure root javadoc process to compile and consume this project's javadocs
+            Javadoc rootJavadoc = project.rootProject.getTasks().getByName("javadoc") as Javadoc
+            Javadoc subJavadoc = project.getTasks().getByName('javadoc') as Javadoc
+            rootJavadoc.setSource(subJavadoc.source)
+            rootJavadoc.classpath += project.files(project.sourceSets.main.compileClasspath)
+        }
+    }
+
+    /**
+     * Add this module's jar output to the root project's master zip distribution.
+     * @param project to be configured
+     */
+    private static void configureProjectZip(Project project) {
+        // We do this after evaluation since the scala projects may change around what the final archive name is.
+        project.afterEvaluate {
+            Zip rootDistZip = project.rootProject.getTasks().getByName('distZip') as Zip
+            rootDistZip.dependsOn(project.getTasks().pack)
+
+            project.getTasks().withType(Jar.class).each { Jar jarTask ->
+                // Add jar output under the dist directory
+                rootDistZip.from(jarTask.archivePath) { CopySpec copySpecification ->
+                    copySpecification.into("${project.rootProject.ext.folderName}/dist")
+                    copySpecification.setDuplicatesStrategy(DuplicatesStrategy.WARN)
+                }
+            }
+        }
+    }
+
+    /**
+     * For all the dependencies set on this project, set them on the root project as well.
+     * @param project to be configured
+     */
+    private static void configureRootProjectDependencies(Project project) {
+        // We do this in an after evaluate so that we pick up all dependencies set after the plugin was configured.
+        // If this becomes a problem, we could see if there's a way to listen for new dependencies and add them
+        // to root at the same time.
+        project.afterEvaluate {
+            project.getConfigurations().getByName('provided').getAllDependencies()
+                .withType(ExternalDependency.class)
+                .each { Dependency dependency ->
+                    // Convert the scope to optional on the root project - it will have every integration in it, and
+                    // users may not need every dependency (except hadoop and jackson)
+                    String scope = (dependency.group in ['org.apache.hadoop', 'org.codehaus.jackson'] ? 'provided' : 'optional')
+                    project.rootProject.getDependencies().add(scope, dependency)
+                }
+
+            project.getConfigurations().getByName('optional').getAllDependencies()
+                .withType(ExternalDependency.class)
+                .each { Dependency dependency ->
+                    // Convert the scope to optional on the root project - it will have every integration in it, and
+                    // users may not need every dependency (except hadoop and jackson)
+                    String scope = (dependency.group in ['org.apache.hadoop', 'org.codehaus.jackson'] ? 'provided' : 'optional')
+                    project.rootProject.getDependencies().add(scope, dependency)
+                }
+        }
+    }
+}
