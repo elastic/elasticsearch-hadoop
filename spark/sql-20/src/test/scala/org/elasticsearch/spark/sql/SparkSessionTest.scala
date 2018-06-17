fd1d63f30d21ff1de5e41018a1adfe012bd10a99
hanbj <hanbj0707@163.com>
2018-06-17 09:32:04 +0800

    change test

--- spark/sql-20/src/test/scala/org/elasticsearch/spark/sql/SparkSessionTest.scala
+++ spark/sql-20/src/test/scala/org/elasticsearch/spark/sql/SparkSessionTest.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.SparkSession
 import org.junit.Assert._
 import org.junit.Test
 
-class EsSparkSQLTest {
+class SparkSessionTest {
 
   @Test
   def parameters(): Unit = {
@@ -13,14 +13,12 @@ class EsSparkSQLTest {
     val session1 = SparkSession.builder().config(sparkConf).getOrCreate()
     session1.conf.set("es.nodes", "127.0.0.1")
     session1.conf.set("es.port", "9200")
+    assertTrue(session1.conf.get("es.nodes").equals("127.0.0.1"))
+    assertTrue(session1.conf.get("es.port").equals("9200"))
 
     val session2 = SparkSession.builder().config(sparkConf).getOrCreate()
     session2.conf.set("es.nodes", "localhost")
     session2.conf.set("es.port", "9201")
-
-    assertTrue(session1.conf.get("es.nodes").equals("127.0.0.1"))
-    assertTrue(session1.conf.get("es.port").equals("9200"))
-
     assertTrue(session2.conf.get("es.nodes").equals("localhost"))
     assertTrue(session2.conf.get("es.port").equals("9201"))
 
