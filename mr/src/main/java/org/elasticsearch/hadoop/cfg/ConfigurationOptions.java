2e3d74eaebbfad1e8d6b5a3a6db02cf26bcfe111
Mark Pavey <mark.pavey@thefilter.com>
2018-11-06 11:53:27 +0000

    Add es.read.shard.preference configuration option to enable Elasticsearch preference query string parameter to be set on searches.

--- mr/src/main/java/org/elasticsearch/hadoop/cfg/ConfigurationOptions.java
+++ mr/src/main/java/org/elasticsearch/hadoop/cfg/ConfigurationOptions.java
@@ -70,6 +70,10 @@ public interface ConfigurationOptions {
     /** Secure Settings Keystore */
     String ES_KEYSTORE_LOCATION = "es.keystore.location";
 
+    /** Elasticsearch shard search preference */
+    String ES_READ_SHARD_PREFERENCE = "es.read.shard.preference";
+    String ES_READ_SHARD_PREFERENCE_DEFAULT = "";
+
     /** Elasticsearch batch size given in bytes */
     String ES_BATCH_SIZE_BYTES = "es.batch.size.bytes";
     String ES_BATCH_SIZE_BYTES_DEFAULT = "1mb";
