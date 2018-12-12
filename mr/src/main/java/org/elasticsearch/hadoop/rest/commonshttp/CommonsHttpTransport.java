539afc5f870400dda80153b05e64ee5499a27c4e
James Baiera <james.baiera@gmail.com>
2018-12-12 18:03:54 -0500

    Emit a deprecation warning when connecting with SOCKS
    relates #1223

--- mr/src/main/java/org/elasticsearch/hadoop/rest/commonshttp/CommonsHttpTransport.java
+++ mr/src/main/java/org/elasticsearch/hadoop/rest/commonshttp/CommonsHttpTransport.java
@@ -357,6 +357,7 @@ public class CommonsHttpTransport implements Transport, StatsAware {
 
         // we actually have a socks proxy, let's start the setup
         if (StringUtils.hasText(proxyHost)) {
+            log.warn("Connecting to Elasticsearch through SOCKS proxy is deprecated in 6.6.0 and will be removed in a later release.");
             proxyInfo = proxyInfo.concat(String.format("[SOCKS proxy %s:%s]", proxyHost, proxyPort));
 
             if (!StringUtils.hasText(proxyUser)) {
