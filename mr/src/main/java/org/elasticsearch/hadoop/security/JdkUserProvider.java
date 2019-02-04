9aed7e7ab5b6bf6a800a209dde8a07fbf1f0b24d
James Baiera <james.baiera@gmail.com>
2019-02-04 10:53:03 -0500

    Add trace statements to JdkUserProvider

--- mr/src/main/java/org/elasticsearch/hadoop/security/JdkUserProvider.java
+++ mr/src/main/java/org/elasticsearch/hadoop/security/JdkUserProvider.java
@@ -23,18 +23,30 @@ import java.security.AccessControlContext;
 import java.security.AccessController;
 import javax.security.auth.Subject;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Operates by retrieving the currently set subject in a way that ES-Hadoop understands, or
  * a blank subject if there isn't currently one available.
  */
 public class JdkUserProvider extends UserProvider {
 
+    private static final Log LOG = LogFactory.getLog(JdkUserProvider.class);
+
     @Override
     public User getUser() {
         AccessControlContext acc = AccessController.getContext();
         Subject subject = Subject.getSubject(acc);
         if (subject == null) {
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("Could not locate existing Subject - Creating new one");
+            }
             subject = new Subject();
+        } else {
+            if (LOG.isTraceEnabled()) {
+                LOG.trace("Located existing Subject - " + subject);
+            }
         }
         return new JdkUser(subject, getSettings());
     }
