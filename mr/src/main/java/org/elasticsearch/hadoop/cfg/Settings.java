f217a768447d6b81b35d253fb5c9d9a7cb90fdba
Keith Massey <keith.massey@elastic.co>
2022-01-19 15:21:04 -0600

    Removing newlines from X-Opaque-ID header

--- mr/src/main/java/org/elasticsearch/hadoop/cfg/Settings.java
+++ mr/src/main/java/org/elasticsearch/hadoop/cfg/Settings.java
@@ -768,7 +768,7 @@ public abstract class Settings {
     public abstract Properties asProperties();
 
     public Settings setOpaqueId(String opaqueId) {
-        setProperty(ES_NET_HTTP_HEADER_OPAQUE_ID, opaqueId);
+        setProperty(ES_NET_HTTP_HEADER_OPAQUE_ID, opaqueId.replace("\n", "").replace("\r", ""));
         return this;
     }
 
