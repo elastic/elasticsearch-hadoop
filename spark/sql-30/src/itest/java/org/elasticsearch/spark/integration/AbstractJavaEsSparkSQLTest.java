132a7cfbc08fa00e7311644699fdb52b88e27fe5
Keith Massey <keith.massey@elastic.co>
2022-01-25 11:20:17 -0600

    Removing unused code

--- spark/sql-30/src/itest/java/org/elasticsearch/spark/integration/AbstractJavaEsSparkSQLTest.java
+++ spark/sql-30/src/itest/java/org/elasticsearch/spark/integration/AbstractJavaEsSparkSQLTest.java
@@ -19,15 +19,12 @@
 package org.elasticsearch.spark.integration;
 
 import java.io.Serializable;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Timestamp;
 import java.util.List;
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import javax.xml.bind.DatatypeConverter;
@@ -210,35 +207,5 @@ public class AbstractJavaEsSparkSQLTest implements Serializable {
 		});
 
         return sqc.createDataFrame(rowData, schema);
-	}
-
-	/*
-	 * Scala renamed scala.collection.JavaConversions to jdk.CollectionConverters in 2.13.
-	 */
-	public static scala.collection.Map<String, String> propertiesAsScalaMap(Properties props) {
-		Class conversionsClass;
-		try {
-			conversionsClass = Class.forName("scala.collection.JavaConversions");
-		} catch (ClassNotFoundException e) {
-			try {
-				conversionsClass = Class.forName("jdk.CollectionConverters");
-			} catch (ClassNotFoundException classNotFoundException) {
-				throw new RuntimeException("No collection converter class found");
-			}
-		}
-		Method method;
-		try {
-			method = conversionsClass.getMethod("propertiesAsScalaMap", Properties.class);
-		} catch (NoSuchMethodException e) {
-			throw new RuntimeException("No propertiesAsScalaMap method on " + conversionsClass);
-		}
-		try {
-			Object result = method.invoke(null, props);
-			return (scala.collection.Map<String, String>) result;
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException(e);
-		} catch (InvocationTargetException e) {
-			throw new RuntimeException(e);
-		}
 	}
 }
