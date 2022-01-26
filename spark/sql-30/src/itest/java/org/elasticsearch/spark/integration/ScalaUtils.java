79afc47ae3ab1ad2c14ae1937f8c5296326ff8e2
Keith Massey <keith.massey@elastic.co>
2022-01-25 18:05:57 -0600

    Removing reflection code that did not work

--- spark/sql-30/src/itest/java/org/elasticsearch/spark/integration/ScalaUtils.java
+++ spark/sql-30/src/itest/java/org/elasticsearch/spark/integration/ScalaUtils.java
@@ -1,38 +1,21 @@
 package org.elasticsearch.spark.integration;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.List;
+import scala.collection.mutable.Map;
+import scala.collection.mutable.HashMap;
+
 import java.util.Properties;
 
 public class ScalaUtils {
     /*
-     * Scala renamed scala.collection.JavaConversions to jdk.CollectionConverters in 2.13.
+     * Scala removed scala.collection.JavaConversions.propertiesAsScalaMap() in 2.13, replacing it with an implicit
+     * jdk.CollectionConverters.asScala. This is an attempt to get a method that works in 2.10-2.13. It can be removed and replaced with
+     * jdk.CollectionConverters once we no longer support scala older than 2.13.
      */
     public static scala.collection.Map<String, String> propertiesAsScalaMap(Properties props) {
-        Class conversionsClass;
-        try {
-            conversionsClass = Class.forName("scala.collection.JavaConversions");
-        } catch (ClassNotFoundException e) {
-            try {
-                conversionsClass = Class.forName("jdk.CollectionConverters");
-            } catch (ClassNotFoundException classNotFoundException) {
-                throw new RuntimeException("No collection converter class found");
-            }
+        Map scalaMap = new HashMap();
+        for (java.util.Map.Entry<Object, Object> entry : props.entrySet()) {
+            scalaMap.put(entry.getKey(), entry.getValue());
         }
-        Method method;
-        try {
-            method = conversionsClass.getMethod("propertiesAsScalaMap", Properties.class);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException("No propertiesAsScalaMap method on " + conversionsClass);
-        }
-        try {
-            Object result = method.invoke(null, props);
-            return (scala.collection.Map<String, String>) result;
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
+        return scalaMap;
     }
 }
