05261884b4ca26a425cef55398877b41bf69f122
Keith Massey <keith.massey@elastic.co>
2022-01-28 08:04:20 -0600

    importing assertNull

--- mr/src/test/java/org/elasticsearch/hadoop/serialization/ScrollReaderTest.java
+++ mr/src/test/java/org/elasticsearch/hadoop/serialization/ScrollReaderTest.java
@@ -53,6 +53,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
