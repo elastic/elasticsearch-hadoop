244b723849eb1a70ee5d0be5bafb02d78d4fe1af
James Baiera <james.baiera@gmail.com>
2024-07-01 16:03:48 -0400

    Clean up Spark dependency lists (#2217)
    Clearing out unused dependencies and restricting scope for compile only dependencies.

--- buildSrc/src/main/groovy/org/elasticsearch/hadoop/gradle/BaseBuildPlugin.groovy
+++ buildSrc/src/main/groovy/org/elasticsearch/hadoop/gradle/BaseBuildPlugin.groovy
@@ -164,7 +164,6 @@ class BaseBuildPlugin implements Plugin<Project> {
     private static void configureRepositories(Project project) {
         project.repositories.mavenCentral()
         project.repositories.maven { url "https://clojars.org/repo" }
-        project.repositories.maven { url 'https://repo.spring.io/plugins-release-local' }
 
         // For Elasticsearch snapshots.
         project.repositories.maven { url "https://snapshots.elastic.co/maven/" } // default
